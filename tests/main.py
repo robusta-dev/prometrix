a4d681bcd7ead5cb1055d7c211767fadad7dbfd5
avi robusta <avi@robusta.dev>
2023-07-31 13:56:41 +0300

    added new version to update pip readme and improved manual tests

--- tests/main.py
+++ tests/main.py
@@ -1,10 +1,12 @@
 import json
+from datetime import datetime, timedelta
 from typing import Dict
-
+from pydantic import ValidationError
 import yaml
 
-from prometrix import (AWSPrometheusConfig, CoralogixPrometheusConfig,
-                       PrometheusConfig, PrometheusNotFound, AzurePrometheusConfig,
+from prometrix import (AWSPrometheusConfig, AzurePrometheusConfig,
+                       CoralogixPrometheusConfig, PrometheusConfig,
+                       PrometheusNotFound, PrometheusQueryResult,
                        get_custom_prometheus_connect)
 
 
@@ -20,13 +22,34 @@ def generate_prometheus_config(
     return PrometheusConfig(**params)
 
 
+def check_result_not_empty(result: PrometheusQueryResult) -> bool:
+    if result.result_type != "matrix":
+        print(f"Prometheus tests for results of type {result.result_type} not supported yet")
+        return False
+    for series in result.series_list_result:
+        if len(series.values) > 1 and len(series.timestamps) > 1:
+            return True
+    return False
+
+
 def run_test(test_type: str, config: PrometheusConfig):
     try:
         prom_cli = get_custom_prometheus_connect(config)
         prom_cli.check_prometheus_connection()
+        result = prom_cli.custom_query_range(
+            query="container_memory_working_set_bytes",
+            start_time=datetime.now() - timedelta(days=1),
+            end_time=datetime.now(),
+            step="5m",
+        )
+        formatted_result = PrometheusQueryResult(data=result)
+        if not check_result_not_empty(formatted_result):
+            print(f"Test {test_type} failed, empty or invalid results")
         print(f"Test {test_type} passed")
     except PrometheusNotFound:
-        print(f"Test {test_type} failed")
+        print(f"Test {test_type} failed, could not connect to prometheus")
+    except ValidationError:
+        print(f"Test {test_type} failed, results of wrong format")
 
 
 def main():
